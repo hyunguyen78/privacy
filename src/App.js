6a4943947edb853f4332a46d644adbd47b804556
hyunguyen78 <huy.nguyenquang78py@gmail.com>
2024-01-27 09:55:17 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header>
+        <h1>
+          Chính Sách Quyền Riêng Tư cho Ứng Dụng "Máy Quét và Đọc QR Code PRO"
+        </h1>
       </header>
+      <main>
+        <p>
+          Chào mừng bạn đến với ứng dụng "Máy Quét và Đọc QR Code PRO"! Dưới đây
+          là chính sách quyền riêng tư của chúng tôi:
+        </p>
+        <p>
+          <strong>Không Thu Thập Thông Tin Cá Nhân</strong>
+        </p>
+        <p>
+          Chúng tôi cam kết không thu thập bất kỳ thông tin cá nhân nào từ người
+          dùng của ứng dụng "Máy Quét và Đọc QR Code PRO". Ứng dụng này được
+          thiết kế để giúp bạn quét và đọc mã QR mà không yêu cầu bất kỳ thông
+          tin cá nhân nào.
+        </p>
+        <p>
+          <strong>Quyền Lợi Người Dùng</strong>
+        </p>
+        <p>
+          Người dùng có quyền sử dụng các tính năng của ứng dụng mà không lo
+          lắng về việc thông tin cá nhân của họ sẽ được thu thập hay chia sẻ.
+        </p>
+        <p>
+          <strong>Bảo Mật Thông Tin</strong>
+        </p>
+        <p>
+          Chúng tôi cam kết bảo vệ mọi thông tin bạn nhập vào ứng dụng một cách
+          an toàn và bảo mật. Thông tin bạn nhập sẽ chỉ được lưu trữ trên thiết
+          bị của bạn và sẽ không bao giờ được chia sẻ với bất kỳ bên thứ ba nào.
+        </p>
+        <p>
+          <strong>Hoạt Động Offline</strong>
+        </p>
+        <p>
+          Ứng dụng "Máy Quét và Đọc QR Code PRO" hoạt động hoàn toàn offline và
+          không yêu cầu kết nối internet để thực hiện các tính năng cơ bản của
+          nó.
+        </p>
+      </main>
+      <footer>
+        <p>
+          Liên hệ chúng tôi:{" "}
+          <a href="mailto:huy.nguyenquang78py@gmail.com">
+            huy.nguyenquang78py@gmail.com
+          </a>
+        </p>
+      </footer>
     </div>
   );
-}
+};
 
 export default App;
